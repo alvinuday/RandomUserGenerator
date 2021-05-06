ad0eddd7943e257d2f335869fd426ccd0a42ee93
Alvin Adarsh <udayalvin@gmail.com>
2021-05-07 02:04:40 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      firstName: "Alvin",
+      lastName: "Adarsh",
+      email: "udayalvin@gmail.com",
+      gender: "male",
+      picture: "ALVIN.jpeg",
+    };
+  },
+
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+      console.log(results);
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
+});
+
+app.mount("#app");
